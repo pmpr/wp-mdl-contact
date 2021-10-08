b02bcf8110a49d905bf468df220745f3ffe0c801
Pmnmpr <pmnmpr@gmail.com>
2021-10-08 13:31:18 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             6160480612695             |
+    |_______________________________________|
+*/
+ use Pmpr\Package\Contact\Contact; Contact::symcgieuakksimmu();
---
+++ src/Pmpr/Package/Contact/Widget/Widget.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             6160480612695             |
+    |_______________________________________|
+*/
+ namespace Pmpr\Package\Contact\Widget; use Pmpr\Package\Contact\Container; class Widget extends Container { public function __construct() { parent::__construct(); $this->iemaakgqgqosiecm(); } public function iemaakgqgqosiecm() { Social::symcgieuakksimmu(); Information::symcgieuakksimmu(); } }
